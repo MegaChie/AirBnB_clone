fd3b457a3590d96f10e959b7b159fe2af7a097c1
MegaChie <workspace.mega@gmail.com>
2023-03-12 01:39:35 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,6 @@
 """ FileStorage """
 
 
-# from models.engine.file_storage import FileStorage
-# storage = FileStorage()
-# storage.reload()
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
+storage.reload()
