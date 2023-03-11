5850229dca5a6b5730ea80f113271fa6c572363b
MegaChie <workspace.mega@gmail.com>
2023-03-12 00:45:09 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -29,8 +29,8 @@ class BaseModel:
 
     def __str__(self):
         """ BaseModel """
-        return ("[{}] ({}) {}".format(type(self).__name__,
-                                  self.id, self.__dict__))
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """ BaseModel """
