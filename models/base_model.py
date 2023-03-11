d83187328a975835bc016611fbc3d10a653fc499
MegaChie <workspace.mega@gmail.com>
2023-03-12 01:40:37 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 
 import uuid
 from datetime import datetime
-# from models import storage
+from models import storage
 
 
 class BaseModel:
@@ -24,7 +24,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            # storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """ BaseModel """
@@ -34,7 +34,7 @@ class BaseModel:
     def save(self):
         """ BaseModel """
         self.updated_at = datetime.now()
-        # storage.save()
+        storage.save()
 
     def to_dict(self):
         """ BaseModel """
