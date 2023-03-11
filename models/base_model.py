37df709ccb534b4d6b31618894228f653cc3014a
MegaChie <workspace.mega@gmail.com>
2023-03-12 00:30:07 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ from models import storage
 class BaseModel:
     """ BaseModel """
     def __init__(self, *args, **kwargs):
-        if kwargs is not None and  kwargs != {}:
+        if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
                     self.__dict__["created_at"] = datetime.strptime(
@@ -19,17 +19,17 @@ class BaseModel:
                     self.__dict__["updated_at"] = datetime.strptime(
                         kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    self.__dict__ [key] = kwargs[key] 
+                    self.__dict__[key] = kwargs[key] 
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-    
+
     def __str__(self):
         """ BaseModel """
         return ("[{}] ({}) {}".format(type(self).__name__,
                                       self.id, self.__dict__))
-        
+
     def save(self):
         """ BaseModel """
         self.update_at = datetime.now()
