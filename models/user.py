f83cadbcc04c1325bb9621bd3d3248c765084bbe
MegaChie <workspace.mega@gmail.com>
2023-03-11 16:06:09 -0800

    Create user.py

---
+++ models/user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+from models.base_model import BaseModel
+class User(BaseModel):
+    """ First User """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
