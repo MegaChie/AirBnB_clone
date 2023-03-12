d0fe97d21ce25a29c9d4a4d71b80d58f104a0222
MegaChie <workspace.mega@gmail.com>
2023-03-12 03:01:19 -0700

    Create console.py

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Console """
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    cmd.PROMPT = "(hbnb) "
+    def do_quit(self, line):
+        """ exit the program """
+        return True
+
+    def do_EOF(self, line):
+        """ exit the program """
+        return True
+
+    def emptyline(self):
+        """ does nothing """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
