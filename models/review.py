5664934db8994dad4716a020289b3c8ff89e1880
MegaChie <workspace.mega@gmail.com>
2023-03-11 23:12:37 -0800

    Create review.py

---
+++ models/review.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" More classes! """
+from models.base_model import BaseModel
+class Review(BaseModel):
+    """ More classes! """
+    place_id = ""
+    user_id = ""
+    text = ""
