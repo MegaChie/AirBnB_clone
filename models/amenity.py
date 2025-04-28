501ca73be2ea6f4ac17097218d694cc046b4dfd3
MegaChie <113614073+MegaChie@users.noreply.github.com>
2025-04-28 13:45:54 +0000

    Starts rebuild

--- models/amenity.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" More classes! """
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """ More classes! """
-    name = ""
--- models/state.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" More classes! """
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """ More classes! """
-    name = ""
