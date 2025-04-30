cc40ef3577c193a4c1a614f026d93287871c9902
MegaChie <113614073+MegaChie@users.noreply.github.com>
2025-04-30 18:24:07 +0000

    Adds JSON serialization and deserialization in the FileStorage class

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ Logic for the base model.
 """
 import uuid
 from datetime import datetime
+from . import storage
 
 
 class BaseModel:
@@ -30,6 +31,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -46,6 +48,7 @@ class BaseModel:
         with the current datetime.
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Logic for the storage model.
+"""
+import json
+
+
+class FileStorage:
+    """
+    Serializes instances to a JSON file and
+    deserializes JSON file to instances.
+    """
+    __file_path = "objects.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Returns the dictionary __objects.
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        Sets in __objects the obj with key <obj class name>.id.
+        """
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to the JSON file.
+        """
+        to_write = {}
+        for key, value in FileStorage.__objects.items():
+            to_write[key] = value.to_dict()
+        with open(FileStorage.__file_path, "w") as cursor:
+            json.dump(to_write, cursor)
+
+    def reload(self):
+        """
+        Deserializes the JSON file to __objects.
+        """
+        try:
+            with open(FileStorage.__file_path, "r") as cursor:
+                objects = json.load(cursor)
+                for key, value in objects.items():
+                    class_name = value["__class__"]
+                    module = __import__("models.base_model",
+                                        fromlist=[class_name])
+                    cls = getattr(module, class_name)
+                    FileStorage.__objects[key] = cls(**value)
+        except Exception:
+            pass
