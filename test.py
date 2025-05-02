2dd22e4fb1141387915676637f4ec114b3a69b97
MegaChie <113614073+MegaChie@users.noreply.github.com>
2025-05-02 19:15:59 +0000

    Updates .gitignore to include user's temporary files and adds an interactive interpreter

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+"""Doc
+"""
+from models.engine.tmp_file_storage import *
+from models.engine.tmp_file_storage import FileStorage
 
-bm1 = BaseModel()
-print(bm1)
-bm2 = BaseModel(**bm1.to_dict())
-print(bm2)
-print(bm1.id == bm2.id)
+
+class FileStorage(FileStorage):
+    """Doc
+    """
+
+    __file_path = None
