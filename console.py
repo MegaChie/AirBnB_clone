1843383f3bf907410926f994181bf1a55146259a
MegaChie <113614073+MegaChie@users.noreply.github.com>
2025-05-17 14:10:43 +0000

    Fixes typo

--- console.py
+++ console.py
@@ -125,7 +125,7 @@ class HBNBCommand(cmd.Cmd):
             objects = storage.all()
             for object in objects:
                 temp = object.split(".")
-                if if temp[0] == args[0] and temp[1] == args[1]:
+                if temp[0] == args[0] and temp[1] == args[1]:
                     del objects[object]
                     storage.save()
                     return
