baf38c46ef3dd4175d10d2638e4e7f54e9f64c9a
MegaChie <113614073+MegaChie@users.noreply.github.com>
2025-05-26 15:12:30 +0000

    Refactors command handling in HBNBCommand to improve class validation and error messages by adding a helper method

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 Command line interpreter for the project.
 """
 import cmd
+from typing import Optional
 from models import storage
 from models.amenity import Amenity
 from models.base_model import BaseModel
@@ -25,13 +26,35 @@ class HBNBCommand(cmd.Cmd):
                "User": User
                }
 
-    def do_quit(self, arg):
+    def helper_class_check(self, name: Optional[str] = None,
+                           ID: Optional[str] = None,
+                           check_ID: bool = False) -> bool:
+        """
+        Checks the class and returns output as needed.
+        """
+        if not name and not ID:
+            print("** class name missing **")
+            return True
+
+        if name not in HBNBCommand.classes.keys():
+            print("** class doesn't exist **")
+            return True
+
+        if check_ID:
+            if not ID:
+                print("** instance id missing **")
+                return True
+        
+        return False
+
+
+    def do_quit(self, arg: str) -> None:
         """
         Terminates the interpreter.
         """
         exit()
 
-    def do_EOF(self, arg):
+    def do_EOF(self, arg: str) -> Optional[bool]:
         """
         Handles the EOF signal to exit the interpreter.
         """
@@ -45,24 +68,20 @@ class HBNBCommand(cmd.Cmd):
         """
         pass
 
-    def do_create(self, arg):
+    def do_create(self, arg: str) -> None:
         """
         Creates a new instance of a model object, saves it then prints its ID.
         """
-        if not arg:
-            print("** class name missing **")
-            return
-        if arg not in HBNBCommand.classes.keys():
-            print("** class doesn't exist **")
-            return
-        else:
-            new_object = HBNBCommand.classes[arg]()
-            new_object.save()
-            object_dict = new_object.to_dict()
-            print(object_dict["id"])
+        if self.helper_class_check(arg):
             return
 
-    def do_all(self, arg):
+        new_object = HBNBCommand.classes[arg]()
+        new_object.save()
+        object_dict = new_object.to_dict()
+        print(object_dict["id"])
+        return
+
+    def do_all(self, arg: str) -> None:
         if not arg:
             objects = storage.all()
             objects_list = []
@@ -70,83 +89,70 @@ class HBNBCommand(cmd.Cmd):
                 objects_list.append(str(object))
             print(objects_list)
             return
-        elif arg not in HBNBCommand.classes.keys():
-            print("** class doesn't exist **")
-            return
-        else:
-            objects = storage.all()
-            objects_list = []
-            for key, value in objects.items():
-                if arg in key:
-                    objects_list.append(str(value))
-            print(objects_list)
+
+        if self.helper_class_check(arg):
             return
 
-    def do_show(self, args):
+        objects = storage.all()
+        objects_list = []
+        for key, value in objects.items():
+            if arg in key:
+                objects_list.append(str(value))
+        print(objects_list)
+        return
+
+    def do_show(self, args) -> None:
         """
         Prints the string representation of an instance
         based on the class name and id.
         """
         args = args.split()
-        if not args:
-            print("** class name missing **")
-            return
-        if args[0] not in HBNBCommand.classes.keys():
-            print("** class doesn't exist **")
-            return
-        elif len(args) == 1:
-            print("** instance id missing **")
-            return
-        else:
-            objects = storage.all()
-            for key, value in objects.items():
-                temp = key.split(".")
-                if temp[0] == args[0] and temp[1] == args[1]:
-                    print(str(value))
-                    return
-            print("** no instance found **")
+        if len(args) == 1:
+            args.append("")
+
+        if self.helper_class_check(args[0], args[1], True):
             return
 
-    def do_destroy(self, args):
+        objects = storage.all()
+        for key, value in objects.items():
+            temp = key.split(".")
+            if temp[0] == args[0] and temp[1] == args[1]:
+                print(str(value))
+                return
+        print("** no instance found **")
+        return
+
+    def do_destroy(self, args) -> None:
         """
         Deletes an instance based on the class name and id.
         """
         args = args.split()
-        if not args:
-            print("** class name missing **")
-            return
-        if args[0] not in HBNBCommand.classes.keys():
-            print("** class doesn't exist **")
-            return
-        elif len(args) == 1:
-            print("** instance id missing **")
-            return
-        else:
-            objects = storage.all()
-            for object in objects:
-                temp = object.split(".")
-                if temp[0] == args[0] and temp[1] == args[1]:
-                    del objects[object]
-                    storage.save()
-                    return
-            print("** no instance found **")
+        if len(args) == 1:
+            args.append("")
+        if self.helper_class_check(args[0], args[1], True):
             return
 
-    def do_update(self,args):
+        objects = storage.all()
+        for object in objects:
+            temp = object.split(".")
+            if temp[0] == args[0] and temp[1] == args[1]:
+                del objects[object]
+                storage.save()
+                return
+        print("** no instance found **")
+        return
+
+    def do_update(self,args) -> None:
         """
         Updates an instance based on the class name and id.
         """
         args = args.split()
-        if not args:
-            print("** class name missing **")
+        if len(args) == 1:
+            args.append("")
+        if self.helper_class_check(args[0], args[1], True):
             return
-        if args[0] not in HBNBCommand.classes.keys():
-            print("** class doesn't exist **")
-            return
-        elif len(args) == 1:
-            print("** instance id missing **")
-            return
-        elif len(args) == 2:
+
+        if len(args) == 2:
             print("** attribute name missing **")
             return
         elif len(args) == 3:
@@ -165,7 +171,7 @@ class HBNBCommand(cmd.Cmd):
                     return
             print("** no instance found **")
             return
-        
+
     def default(self, line):
         """
         Handels advantage commands that are like <ClassName>.<command>
@@ -189,7 +195,8 @@ class HBNBCommand(cmd.Cmd):
             print("*** Unknown syntax: {} ***".format(line))
             return
 
-            
+    def do_exeAll(self, arg):
+        self.do_all(arg)
 
 
 
